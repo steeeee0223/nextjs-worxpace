056d20dc8d21a6ca9a74823fd6bf7e01682d354d
steeeee0223 <mr.steven7@gmail.com>
2023-11-29 07:05:33 +0000

    🔧 `deploy` Fix errors

--- app/(platform)/(dashboard)/_component/index.ts
+++ app/(platform)/(dashboard)/_component/index.ts
@@ -1,4 +1,4 @@
 export { default as MobileSidebar } from "./mobile-sidebar";
 export { default as Navbar } from "./navbar";
-export { default as NavItem, type Organization } from "./nav-item";
+export { default as NavItem } from "./nav-item";
 export { default as Sidebar } from "./sidebar";
