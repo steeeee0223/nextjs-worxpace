f72723b65b41c5ec67f873f5763b0a3ead075d88
steeeee0223 <mr.steven7@gmail.com>
2023-12-08 06:14:15 +0000

    🧩 `component` Modfiy dashboard layout

--- app/(platform)/(dashboard)/[role]/layout.tsx
+++ app/(platform)/(dashboard)/[role]/layout.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from "../_component";
 
 const RoleLayout = ({ children }: PropsWithChildren) => {
     return (
-        <main className="pt-20 md:pt-20 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+        <main className="pt-20 md:pt-20 px-4 w-full 2xl:max-w-screen-xl">
             <div className="flex gap-x-7">
                 <div className="w-64 shrink-0 hidden md:block">
                     <Sidebar />
